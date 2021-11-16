4332bcba29be8b71e31cb6616d50075deba5ec72
Ms. D <stacy_dolderer@gfps.k12.mt.us>
2021-11-16 13:15:32 -0700

    11-16

--- Scratch Work Per 5/drawing_samples.py
+++ Scratch Work Per 5/drawing_samples.py
@@ -8,9 +8,16 @@ arcade.set_background_color(arcade.csscolor.SKY_BLUE)
 # Get ready to draw
 arcade.start_render()
 
-# (The drawing code will go here.)
+# Draw a rectangle
+# Left of 0, right of 599
+# Top of 300, bottom of 0
 arcade.draw_lrtb_rectangle_filled(0, 599, 300, 0, arcade.csscolor.GREEN)
+# Tree trunk
+# Center of 100, 320
+# Width of 20
+# Height of 60
 arcade.draw_rectangle_filled(100, 320, 20, 60, arcade.csscolor.SIENNA)
+# Tree top
 arcade.draw_circle_filled(100, 350, 30, arcade.csscolor.DARK_GREEN)
 # Another tree, with a trunk and ellipse for top
 arcade.draw_rectangle_filled(200, 320, 20, 60, arcade.csscolor.SIENNA)
@@ -34,6 +41,7 @@ arcade.draw_polygon_filled(((500, 400),
                             (520, 360)
                             ),
                            arcade.csscolor.DARK_GREEN)
+
 # Draw a sun
 arcade.draw_circle_filled(500, 550, 40, arcade.color.YELLOW)
 
@@ -48,6 +56,7 @@ arcade.draw_line(500, 550, 550, 600, arcade.color.YELLOW, 3)
 arcade.draw_line(500, 550, 550, 500, arcade.color.YELLOW, 3)
 arcade.draw_line(500, 550, 450, 600, arcade.color.YELLOW, 3)
 arcade.draw_line(500, 550, 450, 500, arcade.color.YELLOW, 3)
+
 # Draw text at (150, 230) with a font size of 24 pts.
 arcade.draw_text("Arbor Day - Plant a Tree!",
                  150, 230,
